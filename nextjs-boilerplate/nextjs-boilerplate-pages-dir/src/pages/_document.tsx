9de003b8b124fb900c456a61bc3f907f9d0983bd
syeui <syeui@mz.co.kr>
2023-07-10 22:55:51 +0900

    ADD : Styled-Component 라이브러리 추가 및 적용을 위한 코드 수정

--- nextjs-boilerplate/nextjs-boilerplate-pages-dir/src/pages/_document.tsx
+++ nextjs-boilerplate/nextjs-boilerplate-pages-dir/src/pages/_document.tsx
@@ -1,13 +1,57 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    try {
+      // Run the React rendering logic synchronously
+      ctx.renderPage = () =>
+        originalRenderPage({
+          // Useful for wrapping the whole react tree
+          enhanceApp: (App) => App,
+          // Useful for wrapping in a per-page basis
+          enhanceComponent: (Component) => Component,
+        })
+
+      // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+      const initialProps = await Document.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: [
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>,
+        ],
+      }
+    } finally {
+      sheet.seal()
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
+
+export default MyDocument
