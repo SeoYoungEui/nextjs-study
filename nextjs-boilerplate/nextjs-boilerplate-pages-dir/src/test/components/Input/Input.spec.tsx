ede4deb87d80f985769aad962de61c27a7e41570
syeui <syeui@mz.co.kr>
2023-07-18 22:18:51 +0900

    FEAT : test Case 추가

--- nextjs-boilerplate/nextjs-boilerplate-pages-dir/src/test/components/Input/Input.spec.tsx
+++ nextjs-boilerplate/nextjs-boilerplate-pages-dir/src/test/components/Input/Input.spec.tsx
@@ -1,5 +1,5 @@
 import { Input } from '@/components/Input'
-import { RenderResult, render, screen } from '@testing-library/react'
+import { RenderResult, fireEvent, render, screen } from '@testing-library/react'
 
 /**
  * describe 함수를 사용하여 함수를 모은다. 여기서는 Input Component 의 테스트를 진행한다.
@@ -26,6 +26,34 @@ describe('Input', () => {
     const inputNode = screen.getByLabelText('Test Input') as HTMLInputElement
 
     // Input 요소의 표시가 비어있는지 확인한다.
+    expect(inputNode).toHaveValue('')
+  })
+
+  it('문자 입력 후 내용이 표시되는지 테스트', () => {
+    const inputText = 'Test Input Text'
+    const inputNode = screen.getByLabelText('Test Input') as HTMLInputElement
+
+    //fireEvent 를 사용해 input 요소의 onChange 이벤트를 트리거합니다.
+    fireEvent.change(inputNode, { target: { value: inputText } })
+
+    // input 요소에 입력한 텍스트가 표시되는지 확인한다.
+    expect(inputNode).toHaveValue(inputText)
+  })
+
+  it('reset 버튼이 클릭되면 입력 텍스트가 초기화되는지 테스트', () => {
+    const inputText = 'Test Input Text'
+    const inputNode = screen.getByLabelText('Test Input') as HTMLInputElement
+    fireEvent.change(inputNode, { target: { value: inputText } })
+
+    // 버튼을 받아온다
+    const buttonNode = screen.getByRole('button', {
+      name: 'Reset',
+    }) as HTMLButtonElement
+
+    // 버튼 Click
+    fireEvent.click(buttonNode)
+
+    //input요소가 Reset 됫는지 확인한다.
     expect(inputNode).toHaveValue('')
   })
 })
