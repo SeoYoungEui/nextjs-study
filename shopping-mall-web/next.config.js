f41aab8518db20e14e216c7f572e202764a9eb29
syeui <syeui@mz.co.kr>
2023-07-25 20:17:09 +0900

    FEAT : 개발 환경을 위한 API 요청 프락시 설정

--- shopping-mall-web/next.config.js
+++ shopping-mall-web/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: () => {
+  compiler: (() => {
     let compilerConfig = {
       styledComponents: true,
     }
@@ -12,6 +12,16 @@ const nextConfig = {
       }
     }
     return compilerConfig
+  })(),
+  // 교차 출처 리소스 공유(CORS) 에서의 쿠키 전송을 피하기 위해 Next.js 의 Rewrites 기능을 사용해 프락시를 설정합니다.
+  // Next.js 의 엔트포인트에 요청을 전송하면 json-server 라는 엔드포인트로 변환되어 요청이 전송됩니다.
+  async rewrites() {
+    return [
+      {
+        source: `${process.env.NEXT_PUBLIC_API_PATH}/:match*`,
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ]
   },
 }
 
